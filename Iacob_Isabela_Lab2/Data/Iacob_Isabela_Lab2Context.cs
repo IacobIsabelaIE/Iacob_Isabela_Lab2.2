59bf7739916eac2753801effd950376823f3e995
IacobIsabelaIE <isabela.iacob@stud.ubbcluj.ro>
2023-11-12 21:52:12 +0200

    commit lab3

--- Iacob_Isabela_Lab2/Data/Iacob_Isabela_Lab2Context.cs
+++ Iacob_Isabela_Lab2/Data/Iacob_Isabela_Lab2Context.cs
@@ -19,5 +19,7 @@ namespace Iacob_Isabela_Lab2.Data
         public DbSet<Iacob_Isabela_Lab2.Models.Publisher> Publisher { get; set; } = default!;
 
         public DbSet<Iacob_Isabela_Lab2.Models.Author> Author { get; set; } = default!;
+
+        public DbSet<Iacob_Isabela_Lab2.Models.Category> Category { get; set; } = default!;
     }
 }
--- Iacob_Isabela_Lab2/Models/Book.cs
+++ Iacob_Isabela_Lab2/Models/Book.cs
@@ -19,7 +19,9 @@ namespace Iacob_Isabela_Lab2.Models
         public DateTime PublishingDate { get; set; }
 
         public int? PublisherID { get; set; }
-        public Publisher? Publisher { get; set; } 
+        public Publisher? Publisher { get; set; }
+
+        public ICollection<BookCategory>? BookCategories { get; set; }
 
     }
 }
